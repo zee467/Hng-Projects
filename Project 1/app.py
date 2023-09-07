9c0d8b2cdfbc058492aab7cde4959eb8e093f000
zee467 <wura.mahmud@gmail.com>
2023-09-07 19:31:23 +0100

    Updated Github File and Repo Url

--- Project 1/app.py
+++ Project 1/app.py
@@ -24,13 +24,16 @@ def index():
     dt_local = current_utc.astimezone(local_zone)
     formatted_utc_time = dt_local.strftime('%Y-%m-%dT%H:%M:%SZ')
 
+    github_file_url = "https://github.com/zee467/Hng-Projects/blob/main/Project%201/app.py"
+    github_repo_url = "https://github.com/zee467/Hng-Projects/tree/main"
+
     data = {
         "slack_name": slack_name,
         "current_day": current_day,
         "utc_time": formatted_utc_time,
         "track": track,
-        "github_file_url": "",
-        "github_repo_url": "",
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
         "status_code": 200
     }
     return jsonify(data)
